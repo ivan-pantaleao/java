345dad2254c1a83221321c7137082f2bed96c37e
ivan-pantaleao <161838890+ivan-pantaleao@users.noreply.github.com>
2024-06-21 21:39:42 -0300

    Update pesquisaCidades.java

--- pesquisaCidades.java
+++ pesquisaCidades.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-  public class Cidades {
+  public class pesquisaCidades {
     private String nome;
     private int identificador;
     private int qtdeVeiculos;
